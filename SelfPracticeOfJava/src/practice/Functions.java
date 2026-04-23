f7c90e4cf448f6f07911dc3d099778f3d757f6cc
tausif900 <ansaritausif9268@gmail.com>
2026-04-23 21:19:07 +0530

    practice

---
+++ SelfPracticeOfJava/src/practice/Functions.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Functions {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Learn Functions or Methods");
+		greeting();						// called function or invoked
+		System.out.println("Class Over.");
+	}
+
+	public static void greeting() {				// Created function
+		System.out.println("Hello!! I am tausif and I am Learning Java Functions");
+	}
+}
