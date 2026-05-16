dcee1403d9028d08f7d33bdb62c23b74c8cfdb1f
tausif900 <ansaritausif9268@gmail.com>
2026-05-16 21:39:59 +0530

    Practice

--- SelfPracticeOfJava/src/collection/UniqueCharacters.java
+++ SelfPracticeOfJava/src/collection/UniqueCharacters.java
@@ -1,7 +1,19 @@
 package collection;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class UniqueCharacters {
 	public static void main(String[] args) {
+		Set<Character> unique = new HashSet<Character>();
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter a string:");
+		String userString = input.next();
 
+		for (Character character : userString.toCharArray()) {
+			unique.add(character);
+		}
+		System.out.println(unique.size());
 	}
 }
