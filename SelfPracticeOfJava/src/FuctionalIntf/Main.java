4d5878716341b6de15895d5d68bf93d315ca583b
tausif900 <ansaritausif9268@gmail.com>
2026-06-07 13:37:31 +0530

    practice

--- SelfPracticeOfJava/src/FuctionalIntf/Main.java
+++ SelfPracticeOfJava/src/FuctionalIntf/Main.java
@@ -2,16 +2,24 @@ package FuctionalIntf;
 
 public class Main {
 	public static void main(String[] args) {
-		FunctionalInf functionalInf = new FunctionalInf() {
+//		FunctionalInf functionalInf = new FunctionalInf() {
+//
+//			@Override
+//			public boolean isPrime(int num) {
+//				if (num % 2 == 0) {
+//					return true;
+//				}
+//				return false;
+//			}
+//		};
+//		System.out.println(functionalInf.isPrime(5));
 
-			@Override
-			public boolean isPrime(int num) {
-				if (num % 2 == 0) {
-					return true;
-				}
-				return false;
+		FunctionalInf prime = num -> {
+			if (num % 2 == 0) {
+				return true;
 			}
+			return false;
 		};
-		System.out.println(functionalInf.isPrime(5));
+		System.out.println(prime.isPrime(200));
 	}
 }
